0d0280835d7e62f59a8f0776e176a8a18fb97f4b
Xuedixuedi <958190137@qq.com>
2020-12-15 14:57:44 +0800

    finish baseknowledegdata

---
+++ src/main/java/com/mesh/backend/datas/BaseKnowledgeData.java
@@ -0,0 +1,35 @@
+package com.mesh.backend.datas;
+
+import com.mesh.backend.entity.Projectmemos;
+import com.mesh.backend.entity.Teammemos;
+
+/**
+ * 项目知识库和团队知识库直接写一起了，没什么区别
+ *
+ * @author xuedixuedi
+ */
+public class BaseKnowledgeData extends BaseData {
+    public KnowledgeData knowledgeData;
+
+    /**
+     * 项目知识库
+     *
+     * @param projectmemos
+     * @param uploaderName
+     */
+    public BaseKnowledgeData(Projectmemos projectmemos, String uploaderName) {
+        super(true, "");
+        knowledgeData = new KnowledgeData(projectmemos, uploaderName);
+    }
+
+    /**
+     * 团队知识库
+     *
+     * @param teammemos
+     * @param uploaderName
+     */
+    public BaseKnowledgeData(Teammemos teammemos, String uploaderName) {
+        super(true, "");
+        knowledgeData = new KnowledgeData(teammemos, uploaderName);
+    }
+}
