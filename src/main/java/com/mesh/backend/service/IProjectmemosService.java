1ac476e537b4f417c7d621ed27cb66bc5047bb51
aodethri <740950342@qq.com>
2020-12-16 16:32:49 +0800

    finish ProjectmemosController

--- src/main/java/com/mesh/backend/service/IProjectmemosService.java
+++ src/main/java/com/mesh/backend/service/IProjectmemosService.java
@@ -1,7 +1,11 @@
 package com.mesh.backend.service;
 
+import com.mesh.backend.datas.KnowledgeData;
+import com.mesh.backend.datas.KnowledgeRequestData;
 import com.mesh.backend.entity.Projectmemos;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +16,9 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2020-12-05
  */
 public interface IProjectmemosService extends IService<Projectmemos> {
+    Projectmemos createNewProjectMemo(KnowledgeRequestData requestData, int userId);
 
+    List<KnowledgeData> queryProjectMemo(int projectId);
+
+    Projectmemos updateProjectMemo(KnowledgeRequestData requestData);
 }
