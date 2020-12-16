4f032a3f61c39236e4d3c2442109f4fc1bd5f0a3
Xuedixuedi <958190137@qq.com>
2020-12-16 16:14:00 +0800

    add bulletin

--- src/main/java/com/mesh/backend/datas/BaseBulletinData.java
+++ src/main/java/com/mesh/backend/datas/BaseBulletinData.java
@@ -2,6 +2,8 @@ package com.mesh.backend.datas;
 
 import com.mesh.backend.entity.Bulletins;
 
+import java.util.List;
+
 /**
  * bulletin返回值
  *
@@ -9,10 +11,26 @@ import com.mesh.backend.entity.Bulletins;
  */
 public class BaseBulletinData extends BaseData {
     public BulletinData bulletin;
+    public List<BulletinData> bulletins;
 
+    /**
+     * 创建
+     *
+     * @param bulletins
+     */
     public BaseBulletinData(Bulletins bulletins) {
         super(true, "");
         bulletin = new BulletinData(bulletins);
+    }
+
+    /**
+     * 查询
+     *
+     * @param bulletins
+     */
+    public BaseBulletinData(List<BulletinData> bulletins) {
+        super(true, "");
+        this.bulletins = bulletins;
     }
 
 }
