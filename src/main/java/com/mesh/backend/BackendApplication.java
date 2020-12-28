c93bd43cd2e8c0d5f1eb84a0ef0cf8064248f1b9
aodethri <740950342@qq.com>
2020-12-28 17:59:47 +0800

    add session

--- src/main/java/com/mesh/backend/BackendApplication.java
+++ src/main/java/com/mesh/backend/BackendApplication.java
@@ -3,8 +3,10 @@ package com.mesh.backend;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
 
 @SpringBootApplication
+@EnableSpringHttpSession
 @MapperScan("com.mesh.backend.mapper")
 public class BackendApplication {
 
